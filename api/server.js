e52ddf6dfb926091020005b07983c2616b73a4c3
omegus100 <121273757+omegus100@users.noreply.github.com>
2025-04-09 11:37:18 -0700

    Vercel Updates

--- api/server.js
+++ api/server.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const app = express()
-const port = process.env.PORT || 5000
 const path = require('path')
 
+const app = express()
+const port = process.env.PORT || 5000
+
+// Middleware
 app.use(express.static('public'))
-app.use(express.json({ limit: '10mb' })); // Adjust the limit as needed
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json({ limit: '10mb' }))
+app.use(express.urlencoded({ limit: '10mb', extended: true }))
 
 // MongoDB connection URI (replace with your actual URI)
 const mongoURI = 'mongodb://localhost:27017/mydatabase'
@@ -16,16 +18,21 @@ mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected successfully'))
   .catch(err => console.error('MongoDB connection error:', err))
 
-//Routes for React server 
-const bookRouter = require('./server/routes/books')
-const authorRouter = require('./server/routes/authors')
-const seriesRouter = require('./server/routes/series')
+// Routes
+const bookRouter = require('../server/routes/books')
+const authorRouter = require('../server/routes/authors')
+const seriesRouter = require('../server/routes/series')
 
-app.use('/api/books', bookRouter) 
+app.use('/api/books', bookRouter)
 app.use('/api/authors', authorRouter)
 app.use('/api/series', seriesRouter)
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')))
 
+// Serve React app
+app.use(express.static(path.join(__dirname, '../client/build')))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
+})
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`))
