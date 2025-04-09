5f85d4f8bef802f93d6b082844b2c7447c365978
omegus100 <121273757+omegus100@users.noreply.github.com>
2025-04-09 13:55:25 -0700

    Serverjs routing fix

--- api/server.js
+++ api/server.js
@@ -30,7 +30,7 @@ app.use('/uploads', express.static(path.join(__dirname, '../uploads')))
 
 // Serve React app
 app.use(express.static(path.join(__dirname, '../client/build')))
-app.get('*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
 })
 
