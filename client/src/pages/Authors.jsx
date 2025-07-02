6314b875d08c2369867bc3109055deabcea5fee1
retromanufacturing <121273757+omegus100@users.noreply.github.com>
2025-07-02 10:33:21 -0700

    Update Authors.jsx

--- client/src/pages/Authors.jsx
+++ client/src/pages/Authors.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { GetData } from '../hooks/getData'
 import { AddButton } from '../components/Buttons'
